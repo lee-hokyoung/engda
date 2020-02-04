57b9481fd15e327fdfc6ff5695d132596f8f5648
super <supereggsong@gmail.com>
2020-02-04 15:12:30 +0900

    이달의 튜터

--- routes/tutors.js
+++ routes/tutors.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("write", { title: "일기 쓰기", active: "write" });
+  res.render("tutors", { title: "잉다 튜터", active: "tutors" });
 });
 module.exports = router;
